7f1907105a352edc8d773a8a47ea5233a8bd2950
aimene belfodil <aimene.belfodil@mobile-devices.fr>
2019-01-29 09:28:48 +0100

    Optimized CbO

--- cbo.py
+++ cbo.py
@@ -3,75 +3,34 @@ from enumerator import Enumerator
 class CbO(Enumerator) :
     def _start(self, *args,**kwargs):
         self.nb_closure_computation = 1 #Computing the closure of the emptyset
-        stack = [(CbOItemset.bottom_itemset(self), 0)]
+        vertical = self.data.vertical
+        m = self.data.m
+        n = self.data.n
+        
+        stack = [(set(range(n)), set([item for item in range(m) if len(vertical[item]) == n]),  0)]
         while stack :
-            itemset, pos = stack.pop()
-            for item in range(pos, self.data.m) :
-                if item in itemset.itemset : continue
-                next_closed = itemset.add_and_close(item)
+            extent, itemset, pos = stack.pop()
+            for item in xrange(pos, self.data.m) :
+                if item in itemset : continue
+
+                new_extent = extent & vertical[item]
                 self.nb_closure_computation+=1
-                if next_closed != None:
-                    stack.append((next_closed, item))
-            yield itemset.itemset, itemset.extent
+                canonicity_test_fail = False
+                for i in xrange(item):
+                    if i in itemset : continue
+                    if vertical[i] >= new_extent :
+                        canonicity_test_fail = True
+                        break
+                if canonicity_test_fail: continue
+
+                new_itemset = set(itemset)
+                new_itemset.add(item)
+                for i in xrange(item+1, m) :
+                    if i in itemset : continue
+                    if vertical[i] >= new_extent : new_itemset.add(i)
+                
+                stack.append((new_extent,new_itemset, item))
+            yield itemset, extent
             
     def __str__(self):
         return "Close-By-One"
-        
-        
-        
-
-
-
-class CbOItemset:
-    def __init__(self, itemset, extent, _enumerator):
-        # _enumerator is the parent enumerator to access as well as the data
-        self._enumerator = _enumerator
-        
-        # The current itemset (set)
-        self.itemset = itemset
-
-        # The extent of the itemset in the database (set)
-        self.extent = extent
-
-    def add(self, item):
-        self.itemset.add(item)
-        self.extent &= self._enumerator.data.vertical[item]
-        return self
-
-    def copy(self):
-        return CbOItemset(set(self.itemset), set(self.extent), self._enumerator)
-
-    def add_and_close(self, item):
-        # return the closure of the current closed itemset union the new item
-        #        or None if any prohibitted element is added (element before item)
-        result = self.copy()
-        result.add(item)
-
-        for i in range(item):
-            if i in self.itemset : continue
-            if self._enumerator.data.vertical[i] >= result.extent : return None
-
-        for i in range(item, self._enumerator.data.m) :
-            if i in self.itemset : continue
-            if self._enumerator.data.vertical[i] >= result.extent : result.add(i)
-
-        return result
-        
-
-    
-
-    @staticmethod
-    def bottom_itemset(_enumerator):
-        # We are always working under the hypothesis that the dataset is column-clarified (column-reduced)
-        extent = set(range(_enumerator.data.n))
-        itemset = set([i for i in range(_enumerator.data.m) if _enumerator.data.vertical[i] == extent])
-        
-        return CbOItemset(itemset, extent, _enumerator)
-        
-        
-    def __str__(self):
-        result = ""
-        result += "itemset = '"+" ".join(map(str,self.itemset))+"'\n"
-        result += "extent = '"+" ".join(map(str,self.extent))+"'\n"
-        return result        
-            
